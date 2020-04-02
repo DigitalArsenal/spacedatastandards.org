0d93c702f96c7e4cd7599bbafbc3f2f3eb085ed3
tjkoury <tjkoury@gmail.com>
2020-04-02 16:09:02 -0400

    updates

--- docs/rollup.config.js
+++ docs/rollup.config.js
@@ -5,14 +5,14 @@ import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
-
+const writePath = !production ? 'dev/build/' : '';
 export default {
 	input: 'src/main.js',
 	output: {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/bundle.js'
+		file: `${writePath}bundle.js`
 	},
 	plugins: [
 		svelte({
@@ -21,7 +21,7 @@ export default {
 			// we'll extract any component CSS out into
 			// a separate file - better for performance
 			css: css => {
-				css.write('public/build/bundle.css');
+				css.write(`${writePath}bundle.css`);
 			}
 		}),
 
@@ -40,9 +40,9 @@ export default {
 		// the bundle has been generated
 		!production && serve(),
 
-		// Watch the `public` directory and refresh the
+		// Watch the `dev` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload('dev'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
