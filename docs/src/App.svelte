ac25247db3b803dc7a5b37a1ff990ae725aadcf4
tjkoury <tjkoury@gmail.com>
2020-04-02 15:54:37 -0400

    update with flatbuffers

--- docs/src/App.svelte
+++ docs/src/App.svelte
@@ -1,30 +1,43 @@
 <script>
-	export let name;
+  import { onMount } from "svelte";
+  import { onLoad } from "./global.js";
+  export let name;
+  export let flatc = { length: null };
+  onLoad(() => {
+    flatc = globalThis.flatc;
+  });
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<svelte:head>
+  <script type="module">
+    import { flatc } from "https://digitalarsenal.io/lib/flatbuffers.js";
+    globalThis.flatc = flatc;
+  </script>
+</svelte:head>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>{flatc.length}</p>
+</main>
