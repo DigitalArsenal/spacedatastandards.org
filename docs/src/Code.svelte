aefffcde13be787504f5222535f04f43f92ebbd2
tjkoury <tj@digitalarsenal.io>
2020-04-08 21:56:22 -0400

    deploy

--- docs/src/Code.svelte
+++ docs/src/Code.svelte
@@ -2,10 +2,32 @@
   import { onMount } from "svelte";
   import { fs } from "./stores/FileSystem";
   import { currentDocument } from "./stores/Route";
- 
+
   let result;
 
-  globalThis.createCode = async () => {
+  let languages = [
+    ["--cpp", "Generate a C++ header", "h"],
+    ["--java", "Generate Java code", "java"],
+    ["--kotlin", "Generate Kotlin code", "kt"],
+    ["--csharp", "Generate C# code", "cs"],
+    ["--go", "Generate Go code", "go"],
+    ["--python", "Generate Python code", "py"],
+    ["--js", "Generate JavaScript code", "js"],
+    ["--ts", "Generate TypeScript code", "ts"],
+    [
+      "--php",
+      " Generate PHP code",
+      "php"
+    ] /*,
+    ["--grpc", " Generate RPC stub code for GRPC", ''],
+    ["--dart", " Generate Dart code"],
+    ["--lua", " Generate Lua code"],
+    ["--lobster", " Generate Lobster code"],
+    ["--rust, -r ", " Generate Rust code"],
+    ["--swift", " Generate Swift code"]*/
+  ];
+  let currentLanguage = languages[0];
+  let createCode = async () => {
     if (!globalThis.flatc) return;
     let fb = new globalThis.flatc({
       fs: fs,
@@ -13,7 +35,7 @@
     });
     await fb.runCommand([
       "./flatc",
-      "--cpp",
+      currentLanguage[0],
       "-o",
       "/test",
       `/test/${$currentDocument}.fbs`
@@ -26,11 +48,18 @@
     window.outPipe.on("data", data => {
       console.log(data.toString("utf8"));
     });
-    result = fs.readFileSync(`/test/${$currentDocument}_generated.h`, {
+    globalThis.fs = fs;
+    let _resultFile;
+    fs.readdirSync("/test/").forEach(f => {
+      if (f.slice(f.lastIndexOf(".") + 1) === currentLanguage[2]) {
+        _resultFile = f;
+      }
+    });
+    result = fs.readFileSync(`/test/${_resultFile}`, {
       encoding: "utf8"
     });
-    //.replace(/\n/g, "<br/>");
   };
+  globalThis.createCode = createCode;
   onMount(() => {
     createCode();
   });
@@ -50,4 +79,9 @@
     createCode();
   </script>
 </svelte:head>
+<select bind:value={currentLanguage} on:change={() => createCode()}>
+  {#each languages as language}
+    <option value={language}>{language[1]}</option>
+  {/each}
+</select>
 <textarea value={result} />
