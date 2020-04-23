2d5ebeedd9b44bda555715fa49ef5b623c4e6969
tjkoury <tjkoury@gmail.com>
2020-04-23 09:04:50 -0400

    iterator work

--- docs/parsers/legacy.js
+++ docs/parsers/legacy.js
@@ -1,6 +1,51 @@
 const utf8Decoder = new TextDecoder("utf-8");
 const leregex = /[\r\n]{1,2}/;
-class tle {};
+async function* iterate(reader) {
+  const utf8Decoder = new TextDecoder("utf-8");
+  let { value: chunk, done } = await reader.read();
+  chunk = chunk ? utf8Decoder.decode(chunk) : "";
+
+  let re = /\r\n|\n|\r/gm;
+  let startIndex = 0;
+  let result;
+
+  for (;;) {
+    let result = re.exec(chunk);
+
+    if (!result) {
+      if (done) {
+        break;
+      }
+      let remainder = chunk.substr(startIndex);
+      ({ value: chunk, done } = await reader.read());
+      chunk = remainder + (chunk ? utf8Decoder.decode(chunk) : "");
+
+      startIndex = re.lastIndex = 0;
+      continue;
+    }
+    yield chunk.substring(startIndex, result.index);
+    startIndex = re.lastIndex;
+  }
+  if (startIndex < chunk.length) {
+    yield chunk.substr(startIndex);
+  }
+}
+
+class lineReader {
+  constructor(reader) {
+    (async function () {
+      for await (let line of iterate(reader)) {
+        console.log("Line", line);
+      }
+    })();
+  }
+}
+
+class tle extends lineReader {
+  constructor(reader) {
+    super(reader);
+  }
+}
 const satcat = null;
 const vcm = null;
 
--- docs/src/components/Examples/Examples.svelte
+++ docs/src/components/Examples/Examples.svelte
@@ -3,45 +3,11 @@
   import { tle, satcat, vcm } from "../../../parsers/legacy.js";
   export let loaded;
   let tles = [];
-  (async function() {
-    async function* makeTextFileLineIterator(fileURL) {
-      const utf8Decoder = new TextDecoder("utf-8");
-      let response = await fetch(fileURL);
-      let reader = response.body.getReader();
-      let { value: chunk, done } = await reader.read();
-      chunk = chunk ? utf8Decoder.decode(chunk) : "";
 
-      let re = /\r\n|\n|\r/gm;
-      let startIndex = 0;
-      let result;
-
-      for (;;) {
-        let result = re.exec(chunk);
-        if (!result) {
-          if (done) {
-            break;
-          }
-          let remainder = chunk.substr(startIndex);
-          ({ value: chunk, done } = await reader.read());
-          chunk = remainder + (chunk ? utf8Decoder.decode(chunk) : "");
-          console.log(remainder);
-          startIndex = re.lastIndex = 0;
-          continue;
-        }
-        yield chunk.substring(startIndex, result.index);
-        startIndex = re.lastIndex;
-      }
-      if (startIndex < chunk.length) {
-        // last line didn't end in a newline char
-        yield chunk.substr(startIndex);
-      }
-    }
-
-    for await (let line of makeTextFileLineIterator("./test/all.txt")) {
-      //console.log("Line", line);
-    }
-  })();
-  onMount(() => {
+  onMount(async () => {
+    let response = await fetch("./test/all.txt");
+    let reader = response.body.getReader();
+    let tles = new tle(reader);
     loaded = true;
   });
 </script>
