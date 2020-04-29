d96705a1995580df26b7414f97689fb24272a0d0
tjkoury <tj@digitalarsenal.io>
2020-04-29 17:58:25 -0400

    deploy

--- docs/src/components/Examples/Examples.svelte
+++ docs/src/components/Examples/Examples.svelte
@@ -17,10 +17,11 @@
 
   onMount(() => {
     let root_type = $IDLEditorContents.match(/(?<=root_type\s{1,})([^;]{1,})/g);
-    if (root_type) {
+    if (root_type && examples[root_type[0]]) {
       activeComponent = examples[root_type[0]];
     } else {
-      window.location.hash = "/select";
+      alert(`No Example For Type ${root_type}`);
+      window.location.hash = "/idl";
     }
   });
 </script>
