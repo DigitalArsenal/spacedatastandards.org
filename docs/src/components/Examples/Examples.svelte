4a95d035ff8ea1fe8b5048474ed37ccbae2c81b2
tjkoury <tj@digitalarsenal.io>
2020-04-29 17:56:27 -0400

    deploy

--- docs/src/components/Examples/Examples.svelte
+++ docs/src/components/Examples/Examples.svelte
@@ -1,8 +1,32 @@
 <script>
   import OMM from "./OMM.svelte";
+  import { onMount, onDestroy } from "svelte";
+
+  import {
+    CodeEditorDocuments,
+    IDLEditorContents,
+    IDLDocument
+  } from "../../stores/Files";
+
   export let loaded;
   export let args;
   export let toggleMenu;
+
+  let examples = { OMM };
+  let activeComponent;
+
+  onMount(() => {
+    let root_type = $IDLEditorContents.match(/(?<=root_type\s{1,})([^;]{1,})/g);
+    if (root_type) {
+      activeComponent = examples[root_type[0]];
+    } else {
+      window.location.hash = "/select";
+    }
+  });
 </script>
 
-<svelte:component bind:loaded bind:args bind:toggleMenu this={OMM} />
+<svelte:component
+  this={activeComponent}
+  bind:loaded
+  bind:args
+  bind:toggleMenu />
