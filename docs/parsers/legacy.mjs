a66a33019eb8e8a816602345bcb4679cca353423
tjkoury <tj@digitalarsenal.io>
2020-05-06 09:50:02 -0400

    deploy

--- docs/parsers/legacy.mjs
+++ docs/parsers/legacy.mjs
@@ -89,7 +89,7 @@ class tle extends lineReader {
         });
         if (OBJECT_NAME) _OMM.OBJECT_NAME = OBJECT_NAME;
         _OMM.USER_DEFINED_MICROSECONDS = _OMM.EPOCH.microseconds;
-        _OMM.USER_DEFINED_UNIX_TIMESTAMP = _OMM.EPOCH.getTime();
+        _OMM.USER_DEFINED_EPOCH_UNIX_TIMESTAMP = _OMM.EPOCH.getTime();
         return _OMM;
       }
     };
