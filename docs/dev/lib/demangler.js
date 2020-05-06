ed258efafdcdb87214049c6ea3165f8318efb884
tjkoury <tj@digitalarsenal.io>
2020-05-06 14:26:05 -0400

    deploy

--- docs/dev/lib/demangler.js
+++ docs/dev/lib/demangler.js
@@ -1,9 +1,20 @@
 export default (propertyArray = [], text) => {
-  propertyArray.forEach((canonicalname) => {
-    let mangledname = canonicalname.replace(/_/g, "").toUpperCase();
-    let mRegExp = new RegExp(`${mangledname}(?!\\w)`, "g");
-    console.log(mRegExp, canonicalname);
+  let mangledNames = [];
+  for (let i = 0; i < propertyArray.length; i++) {
+    mangledNames.push(propertyArray[i].replace(/_/g, "").toUpperCase());
+  }
+  for (let i = 0; i < mangledNames.length; i++) {
+    let mRegExp = new RegExp(mangledNames[i], "g");
+    for (let ii = i + 1; ii < mangledNames.length; ii++) {
+      mangledNames[ii] = mangledNames[ii].replace(mRegExp, propertyArray[i]);
+    }
+  }
+  propertyArray.forEach((canonicalname, i) => {
+    let mRegExp = new RegExp(mangledNames[i], "g");
     text = text.replace(mRegExp, canonicalname);
+    for (let ii = i + 1; ii < propertyArray.length; ii++) {
+      propertyArray[ii] = propertyArray[ii].replace(mRegExp, canonicalname);
+    }
   });
 
   return text;
