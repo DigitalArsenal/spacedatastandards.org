974b7f41d853064a9885f438369beaec75caee92
tj <tj@digitalarsenal.io>
2022-10-26 15:06:08 -0400

    deploy

--- scripts/convertMarkDown.mjs
+++ scripts/convertMarkDown.mjs
@@ -3,14 +3,14 @@ import { globby } from "globby";
 import { readFileSync, writeFileSync } from "fs";
 
 let converter = new showdown.Converter();
-converter.setOption('tables', true);
+converter.setOption("tables", true);
 
 const paths = await globby([`${process.cwd()}/docs/standards/**/*.md`]);
-paths.forEach(p => {
-    let fileMD = readFileSync(p, "utf-8");
-    let resultHTML = converter.makeHtml(fileMD);
-    writeFileSync(p, fileMD);
-    writeFileSync(p.replace(".md", ".html"), resultHTML);
+paths.forEach((p) => {
+  let fileMD = readFileSync(p, "utf-8");
+  let resultHTML = converter.makeHtml(fileMD);
+  writeFileSync(p, fileMD);
+  writeFileSync(p.replace(".md", ".html"), resultHTML);
 });
 
-console.log(paths);
+console.log("Created Markdown:", paths);
