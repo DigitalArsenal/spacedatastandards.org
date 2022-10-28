4a66b22a79a1a1eaeaea9ff6ebf8f0d3eb0f5d5b
tj <tj@digitalarsenal.io>
2022-10-28 16:18:30 -0400

    deploy

--- src/lib/TopBar.svelte
+++ src/lib/TopBar.svelte
@@ -1,26 +1,35 @@
 <script lang="ts">
-    import { location } from "svelte-spa-router";
-    export let title: string;
+  import { location } from "svelte-spa-router";
+  import Icon from "svelte-awesome";
+  import { github } from "svelte-awesome/icons";
+  export let title: string;
 </script>
 
 <div
-    class="pl-3 pr-3 bg-gray-700 text-white flex items-center justify-between text-xs md:text-xl lg:text-xl font-light"
+  class="pl-3 pr-3 bg-gray-800 text-white flex items-center justify-between text-xs md:text-xl lg:text-xl font-light"
 >
-    <div class="flex gap-2 h-10">
-        <a
-            class:bg-blue-500={$location === "/"}
-            class="transition-all flex gap-2 pl-2 pr-2 items-center justify-start"
-            href="#/"
-        >
-            HOME
-        </a>
-        <a
-            class:bg-blue-500={$location === "/Standards"}
-            class="transition-all p-1 flex gap-2 pl-2 pr-2 items-center justify-start"
-            href="#/Standards"
-        >
-            STANDARDS
-        </a>
-    </div>
-    <div>{@html title}</div>
+  <div class="flex gap-2 h-10">
+    <a
+      class:bg-blue-500={$location === "/"}
+      class="transition-all flex gap-2 pl-2 pr-2 items-center justify-start"
+      href="#/"
+    >
+      HOME
+    </a>
+    <a
+      class:bg-blue-500={$location === "/Standards"}
+      class="transition-all p-1 flex gap-2 pl-2 pr-2 items-center justify-start"
+      href="#/Standards"
+    >
+      STANDARDS
+    </a>
+  </div>
+  <div>{@html title}</div>
+  <div class="flex items-center justify-center">
+    <a
+      class="transition-all p-1 flex gap-2 pl-6 pr-2 items-center justify-start"
+      href="https://github.com/digitalarsenal/spacedatastandards.org"
+      ><Icon data={github} />GITHUB</a
+    >
+  </div>
 </div>
