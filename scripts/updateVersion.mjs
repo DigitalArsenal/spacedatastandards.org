ff5ef238cf003d829b11321941cefef130d49187
tj <tj@digitalarsenal.io>
2023-03-17 15:57:04 -0400

    deploy

--- scripts/updateVersion.mjs
+++ scripts/updateVersion.mjs
@@ -3,5 +3,5 @@ import packageJSON from "../package.json" assert { "type": "json" }
 import packageJSONFB from "../packages/flatbuffers_wasm/package.json" assert { "type": "json" }
 
 let newVersion = Date.now();
-packageJSON.version = `${packageJSONFB.version}+${newVersion}`;
+packageJSON.version = `${packageJSONFB.version}-${packageJSON.version.split(/[\-|\+]/g)[1]}+${newVersion}`;
 writeFileSync("./package.json", JSON.stringify(packageJSON, null, 4));
