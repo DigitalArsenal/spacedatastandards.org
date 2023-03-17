87b73816cbaf11854caa94f8ac0066711ec06814
tj <tj@digitalarsenal.io>
2023-03-17 16:16:08 -0400

    deploy

--- scripts/updateVersion.mjs
+++ scripts/updateVersion.mjs
@@ -1,7 +1,15 @@
 import { readFileSync, writeFileSync } from "fs";
 import packageJSON from "../package.json" assert { "type": "json" }
 import packageJSONFB from "../packages/flatbuffers_wasm/package.json" assert { "type": "json" }
+import { execSync } from "child_process";
 
 let newVersion = Date.now();
-packageJSON.version = `${packageJSONFB.version}-${packageJSON.version.split(/[\-|\+]/g)[1]}+${newVersion}`;
+let mVersion = `${packageJSONFB.version}-${packageJSON.version.split(/[\-|\+]/g)[1]}`;
+
+packageJSON.version = `${mVersion}+${newVersion}`;
+
 writeFileSync("./package.json", JSON.stringify(packageJSON, null, 4));
+
+if (!~execSync("git tag").toString().split("\n").indexOf("v" + mVersion)) {
+    console.log(execSync(`git tag -a v${mVersion} -m "${mVersion}"`).toString());
+}
